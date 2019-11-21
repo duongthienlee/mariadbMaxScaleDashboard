c309f2f09c7ad0411c923b47fecd585a4053a4bb
duongthienlee <duongthienlee@gmail.com>
2019-11-21 11:36:55 +0200

    feat: basisic loop fetch every second draft function

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import config from 'config';
 
 export const LOGIN = 'LOGIN'
 export const LOGIN_OK = 'LOGIN_OK'
@@ -8,7 +9,7 @@ export const LOGIN_FAILED = 'LOGIN_FAILED'
 export function auth(credentials) {
     return dispatch => {
         dispatch({ type: LOGIN, credentials: credentials })
-        return axios.get('http://54.229.207.205:8989/v1/servers', { auth: credentials })
+        return axios.get(`${config.api.root}/v1/servers`, { auth: credentials })
             .then((response) => {
                 const { data } = response
                 //Fake jwt_token
